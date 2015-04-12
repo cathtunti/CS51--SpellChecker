7f007cade6692642b8527730b7ff2ab68153634f
Billie Wei <billiewei@college.harvard.edu>
2015-04-12 13:56:45 -0400

    test

--- project.ml
+++ project.ml
@@ -203,7 +203,7 @@ struct
           let s = String.filter ~f:(fun c -> (c >= 'a' && c <= 'z')) hd in
   *)
 
-  let rec search word t =
+  let rec search word t = 
 
   (********************)
   (* Helper Functions *)
